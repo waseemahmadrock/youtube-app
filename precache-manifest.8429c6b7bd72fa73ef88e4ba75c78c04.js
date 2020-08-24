b873a6e8e6ee52c5c449f00b2938287a4441e200
waseemahmadrock <waseemahmadrock@gmail.com>
2020-08-25 02:34:39 +0500

    Updates

---
+++ precache-manifest.8429c6b7bd72fa73ef88e4ba75c78c04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb29d5b1362fc0b7b92b676482641d9b",
+    "url": "/youtube-app/index.html"
+  },
+  {
+    "revision": "9aa05969d8a35016e404",
+    "url": "/youtube-app/static/css/main.398554a6.chunk.css"
+  },
+  {
+    "revision": "82ffb562276423bd9b6b",
+    "url": "/youtube-app/static/js/2.5a83f94c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-app/static/js/2.5a83f94c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa05969d8a35016e404",
+    "url": "/youtube-app/static/js/main.4edb8104.chunk.js"
+  },
+  {
+    "revision": "7c395697440750f123b0",
+    "url": "/youtube-app/static/js/runtime-main.9d2bbf8f.js"
+  }
+]);
